222d53d1a026e94ee7da6766525505c26ee60bbd
D0ubl3G <d0ubl3g@protonmail.com>
2020-10-24 17:55:00 +0200

    Interactive mode fixes

--- Modules/Interactive.py
+++ Modules/Interactive.py
@@ -2,10 +2,13 @@ import schedule
 from colorama import Fore, Style
 
 from Control import Extraction, Illumination, Ventilation
+from Sensors import Receiver
 
 
 def printMenu():
+    print(Style.BRIGHT)
     print("------------------------------ TERRAFORMER CONTROL ------------------------------")
+    print("| help : Print this menu                                                        |")
     print("| status : Print environment status (Temp, Hum, etc.)                           |")
     print("| extraction : Print extraction system status.                                  |")
     print("|     ... start : Start extraction system.                                      |")
@@ -28,54 +31,66 @@ def printMenu():
 def printStatus():
     print(Style.BRIGHT + Fore.LIGHTBLUE_EX)
     print("-------------- GROW ENVIRONMENT STATUS --------------")
-    print("|       Temperature %fºC    Humidity: %f%    |")
+    print("|       Temperature: " + str(Receiver.TEMP) + "     Humidity: " + str(Receiver.HUM + "%") + "          |")
     print("|---------------------------------------------------|")
+    print(Style.RESET_ALL)
 
 
 def extractionStart():
     Extraction.turnON()
+    print(Style.BRIGHT + Fore.GREEN + "Extraction system turned on." + Style.RESET_ALL)
 
 
 def extractionStop():
     Extraction.turnOFF()
+    print(Style.BRIGHT + Fore.RED + "Extraction system turned off." + Style.RESET_ALL)
 
 
 def extractionAutoStart():
     Extraction.reScheduleAuto()
+    print(Style.BRIGHT + Fore.GREEN + "Extraction autonomous system scheduled." + Style.RESET_ALL)
 
 
 def extractionAutoStop():
     schedule.clear('extraction-auto')
     Extraction.AUTO_SCHEDULED = False
+    print(Style.BRIGHT + Fore.RED + "Extraction autonomous system unscheduled." + Style.RESET_ALL)
 
 
 def illuminationStart():
     Illumination.turnON()
+    print(Style.BRIGHT + Fore.GREEN + "Illumination turned on." + Style.RESET_ALL)
 
 
 def illuminationStop():
     Illumination.turnOFF()
+    print(Style.BRIGHT + Fore.RED + "Illumination turned off." + Style.RESET_ALL)
 
 
 def ventilationStart():
     Ventilation.turnON()
+    print(Style.BRIGHT + Fore.GREEN + "Ventilation system turned on." + Style.RESET_ALL)
 
 
 def ventilationStop():
     Ventilation.turnOFF()
+    print(Style.BRIGHT + Fore.RED + "Ventilation system turned on." + Style.RESET_ALL)
 
 
 def ventilationAutoStart():
     Ventilation.reScheduleAuto()
+    print(Style.BRIGHT + Fore.GREEN + "Ventilation autonomous system scheduled." + Style.RESET_ALL)
 
 
 def ventilationAutoStop():
     schedule.clear('ventilation-auto')
     Ventilation.AUTO_SCHEDULED = False
+    print(Style.BRIGHT + Fore.RED + "Ventilation autonomous system unscheduled." + Style.RESET_ALL)
 
 
 def execute(option):
     switcher = {
+        "help": printMenu,
         "status": printStatus,
         "extraction start": extractionStart,
         "extraction stop": extractionStop,
@@ -88,12 +103,12 @@ def execute(option):
         "ventilation auto start": ventilationAutoStart,
         "ventilation auto stop": ventilationAutoStop
     }
-    func = switcher.get(option, lambda: "Invalid option")
+    func = switcher.get(option, default="help")
     func()
 
 
 def start():
+    printMenu()
     while True:
-        printMenu()
-        option = input("")
+        option = input("=> " + Style.RESET_ALL)
         execute(option)
